f11ea18f7c009fe111b52b5395bdc9eb5783b5fe
andy <andy.wang@yuxisoft.cn>
2018-09-28 16:37:43 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "airbnb",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2016,
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            0,
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ]
+    }
+};
---
+++ src/index.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Andy on 2018/9/28.
+ */
+
+const abc = '123'
+console.log(abc)
